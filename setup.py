ae260182e0b018fefbb6eede5c2f1a957eb7cb14
Damian Yurzola <damian@yurzola.net>
2018-12-24 14:49:59 -0800

    init setup.py

--- setup.py
+++ setup.py
@@ -11,20 +11,23 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "freenas_manager",
-    version = "0.0.1",
-    author = "Andrew Carter",
-    author_email = "andrewjcarter@gmail.com",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "to the cheese shop a5 pypi.org."),
-    license = "BSD",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['an_example_pypi_project', 'tests'],
+    name = 'freenas_manager',
+    version = '0.0.1',
+    author = 'Damian Yurzola',
+    author_email = 'damian@yurzola.net',
+    description = ('Manage freenas uptime'),
+    license = 'MIT',
+    keywords = 'example documentation tutorial',
+    # url = 'http://TBD',
+    install_requires=[
+        'requests',
+        'click',
+        'python-nmap',
+        'pynetgear'
+        ],
     long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
     ],
 )
